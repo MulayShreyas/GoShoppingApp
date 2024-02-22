e60ef100584ee694a209bdd5a397660b91ef95a1
MulayShreyas <shreyasmulay77@gmail.com>
2024-02-22 13:10:21 +0530

    Customer file is modified

--- Customer/customer.java
+++ Customer/customer.java
@@ -12,11 +12,17 @@ public class customer {
     public static void main(String[] args) {
         ShopFactory sf = new GSShopFactory();
 
-        PrimeAcc pa = new GSPrimeAcc(345432344, "Apurva", 4000, true);
+        PrimeAcc pa = new GSPrimeAcc(3, "Ganeh", 400, true);
         pa.bookProduct(pa.getCharges());
         pa.toString();
+        System.out.println("--------------------------------------------------------------");
 
-        NormalAcc na = new GSNormalAcc(543452344, "Ritesh", 50000, 10000);
+        PrimeAcc pa1 = new GSPrimeAcc(4, "Akshay", 250, false);
+        pa1.bookProduct(pa1.getCharges());
+        pa1.toString();
+        System.out.println("--------------------------------------------------------------");
+
+        NormalAcc na = new GSNormalAcc(5, "Ritesh", 300, 50);
         na.bookProduct(na.getdeliveryCharges());
         na.toString();
 
